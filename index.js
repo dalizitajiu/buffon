0584c68cffce82d8da281048d7022f3beab66b28
Jack <573912623@qq.com>
2018-08-26 17:49:25 +0800

    test pm2

--- index.js
+++ index.js
@@ -188,6 +188,7 @@ fastify.register(function (instance, opts, next) {
 //     reply.header('Content-Type','application/json').code(200)
 //     reply.send()
 // })
+console.log("fuck shit")
 fastify.listen(3000, err => {
   if (err) {
     console.log(err)
